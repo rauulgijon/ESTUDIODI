41bd1e24d9046c5f93cae4a21d410ac1194fe9de
Raúl Gijón Sánchez - Maroto <raulgijon16@gmail.com>
2026-06-03 12:24:47 +0200

    dfd

---
+++ Estudio1/Estudio1/CampingDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace Estudio1
+{
+}
+
+namespace Estudio1
+{
+
+
+    public partial class CampingDataSet
+    {
+    }
+}
+namespace Estudio1 {
+    
+    
+    public partial class CampingDataSet {
+    }
+}
